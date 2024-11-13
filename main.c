37e368e465f3d3bad169707dc8efd14bcdf98907
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-11-13 02:45:49 -0300

    feat: book POST endpoint

--- main.c
+++ main.c
@@ -18,6 +18,51 @@ int greeting(const struct _u_request * request, struct _u_response * response, v
     return U_CALLBACK_COMPLETE;
 }
 
+int create_book(const struct _u_request * request, struct _u_response * response, void * user_data) {
+    _book *books = (_book *) user_data;
+    json_t *request_json = ulfius_get_json_body_request(request, NULL);
+    if(request_json == NULL) {
+        printf("Failed to get the response json on /book POST");
+    }
+
+    char *name = (char *) json_string_value(json_object_get(request_json, "name"));
+    char *gender = (char *) json_string_value(json_object_get(request_json, "gender"));
+    int year = json_integer_value(json_object_get(request_json, "year"));
+
+    if(name == NULL) {
+        json_t *json = json_object();
+        json_object_set_new(json, "message", json_string("Name field required!"));
+        ulfius_set_json_body_response(response, 400, json);
+        return U_CALLBACK_COMPLETE;
+    }
+
+    if(gender == NULL) {
+        json_t *json = json_object();
+
+        json_object_set_new(json, "message", json_string("Gender field required!"));
+        ulfius_set_json_body_response(response, 400, json);
+        return U_CALLBACK_COMPLETE;
+    }
+
+    if(year == 0) {
+        json_t *json = json_object();
+
+        json_object_set_new(json, "message", json_string("Year field required"));
+    }
+
+    books[book_counter].id = book_counter;
+    books[book_counter].name = name;
+    books[book_counter].gender = gender;
+    books[book_counter].year = year;
+    book_counter++;
+
+    json_t *response_json = json_object();
+    json_object_set_new(response_json, "Message", json_string("Book added with success"));
+    
+    ulfius_set_json_body_response(response, 200, response_json);
+    return U_CALLBACK_COMPLETE;
+}
+
 int main(void) {
     struct _u_instance instance;
     _book *books = (_book *) malloc(500 * sizeof(_book));
@@ -25,6 +70,8 @@ int main(void) {
     ulfius_init_instance(&instance, PORT, NULL, NULL);
 
     ulfius_add_endpoint_by_val(&instance, "GET", "", NULL, 0, &greeting, NULL);
+
+    ulfius_add_endpoint_by_val(&instance, "POST", "book", "", 0, &create_book, books);
 
     if(ulfius_start_framework(&instance) == U_OK) {
         printf("Server running on port %d...\n", instance.port);
