400a233b8a519259dad39853aab6b936e4c24952
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-11-14 01:33:01 -0300

    put an unexistent book error handling

--- main.c
+++ main.c
@@ -90,8 +90,14 @@ int update_book(const struct _u_request * request, struct _u_response * response
     if(id == NULL) {
         printf("Failed to find the book by id on update_book");
     }
-    
+
+    json_t *response_json = json_object();
     int book_id = atoi(id);
+    if(book_id > book_counter || book_id < 0) {
+        json_object_set_new(response_json, "message", json_string("unexistent book id"));
+        ulfius_set_json_body_response(response, 404, response_json);
+        return U_CALLBACK_COMPLETE;
+    }
     _book *books = (_book *) user_data;
     json_t *request_json = ulfius_get_json_body_request(request, NULL);
     if(request_json == NULL) {
@@ -113,7 +119,6 @@ int update_book(const struct _u_request * request, struct _u_response * response
         books[book_id].year = year;
     }
 
-    json_t *response_json = json_object();
     json_object_set_new(response_json, "message", json_string("Book updated with success"));
     ulfius_set_json_body_response(response, 200, request_json);
 
